f1749a4b7eae7962013616cfc689ce5bd39b6015
AboodXD <aboood40091@gmail.com>
2024-03-13 11:09:19 +0400

    Update makeNewerKP.bat

--- Kamek/makeNewerKP.bat
+++ Kamek/makeNewerKP.bat
@@ -3,13 +3,15 @@ SET mypath=%~dp0
 echo adding paths ''%mypath:~0,-1%\tools\devkitPPC\bin'' and ''%mypath:~0,-1%\tools\NewerSMBW-LLVM\bin''
 set PATH=%PATH%;%mypath:~0,-1%\tools\devkitPPC\bin;%mypath:~0,-1%\tools\NewerSMBW-LLVM\bin
 echo path set.
+rmdir /s /q processed
 rmdir /s /q Build
 rmdir /s /q NewerASM
+mkdir processed
 mkdir Build
 mkdir NewerASM
-C:\Python27\python.exe tools/mapfile_tool.py
-C:\Python27\python.exe tools/kamek.py NewerProjectKP.yaml --show-cmd --no-rels --use-clang --show_cmd --gcc-type=tools/devkitPPC/bin/powerpc-eabi --llvm-path=tools/NewerSMBW-LLVM/bin
-C:\Python27\python.exe tools/renameNewer.py --output=Build
+C:\python27-x64\python.exe tools/mapfile_tool.py
+C:\python27-x64\python.exe tools/kamek.py NewerProjectKP.yaml --show-cmd --no-rels --use-clang --show_cmd --gcc-type=tools/devkitPPC/bin/powerpc-eabi --llvm-path=tools/NewerSMBW-LLVM/bin
+C:\python27-x64\python.exe tools/renameNewer.py --output=Build
 
 
 
